bb6ec8eb9b779e43f70fe496a469017393f31df7
Lulky <lukyluxiano.12@gmail.com>
2022-02-20 19:32:25 -0500

    Evento Onclick

---
+++ javascript/Onclick.js
@@ -0,0 +1,11 @@
+function ninja() {
+    alert("Ninja was liked")
+}
+
+function cambioboton(boton) {
+    boton.innerText = "Logout";
+}
+
+function remove(botn) {
+    botn.style.display ="none"
+}
